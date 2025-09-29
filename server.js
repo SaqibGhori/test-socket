448e9d5e5e44c62f5c056d88739a8330a19ae6d0
Dev4644 <dev4@oip.com.pk>
2025-09-29 15:01:42 +0500

    node

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// Simple Node.js + Socket.IO server
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.get("/", (req, res) => {
+  res.send("Socket.IO server is running 🚀");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
